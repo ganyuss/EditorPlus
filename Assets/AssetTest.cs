fa8dff08940f0731328545dda57884e25c92e846
Clément Couture <clement.couture@beatfitnessgames.com>
2021-09-12 11:04:26 +0200

    Sorted test class

--- Assets/AssetTest.cs
+++ Assets/AssetTest.cs
@@ -1,53 +1,32 @@
+using System;
 using EditorPlus;
 using UnityEngine;
 
 public class AssetTest : ScriptableObject {
-    public int test;
     [CustomSpace(20)]
     [Disabled]
-    public int test2;
+    public int Test;
 
-    public MinMaxInt minMax1;
     [HelpBox("This field is convenient", HelpBoxType.Info)]
     [CustomSpace(20, 10)]
     [MinMaxSlider(0, 20)]
-    public MinMaxFloat minMax2;
-    [MinMaxSlider(0, 20)]
-    public float minMax0;
+    public MinMaxFloat MinMax;
 
-    [CustomSpace(20, 10)]
+    [CustomSpace(10, 10)]
     [TextArea(2, 3)]
-    public string testTextArea;
-    [TextArea(2, 3)]
-    public string comparison;
-    [CustomSpace(20)]
+    public string TextArea;
+    [CustomSpace(10 ,10)]
     [Multiline(5)]
-    public string testMultiline;
-    [Multiline(5)]
-    public string comparison2;
+    public string Multiline;
+    [CustomSpace(10 ,10)]
+    [Range(0, 100)]
+    public float Range;
     
     [Button("test")]
     public void a() {
         Debug.Log("AAAAAAAAAAAAAAA");
     }
-    
-    [Button(Size = ButtonSize.Small)]
-    public void c() {
-        Debug.Log("CCCCCCCCCCCCCCCCCCC");
-    }
-    
-    
-    [Button(Size = ButtonSize.Regular)]
-    public void d() {
-        Debug.Log("DDDDDDDDDDDDDDDDDDDDDDDDD");
-    }
-    
-    
-    [Button(Size = ButtonSize.Large)]
-    public void b() {
-        Debug.Log("BBBBBBBBBBBBBBB");
-    }
-    
+
     [Button(Size = ButtonSize.ExtraLarge)]
     public void e() {
         Debug.Log("EEEEEEEEEEEEEEEEEEEEEE");
