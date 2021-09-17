6223280e8fa43918155d0c11d61ad7294d7943fd
Clément Couture <clement.couture@beatfitnessgames.com>
2021-09-17 16:14:03 +0200

    Fixed one time create asset

--- Assets/EditorPlus/Editor/OneTimeCreateAsset.cs
+++ Assets/EditorPlus/Editor/OneTimeCreateAsset.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
 
@@ -28,41 +25,13 @@ namespace EditorPlus.Editor {
         }
 
         private static Type GetAssetTypeFrom(TextAsset selectedAsset) {
-            List<string> typeNames = GetAllTypeNamesIn(selectedAsset);
-
-            foreach (string typeName in typeNames) {
-                foreach (var type in TypeUtils.GetTypesFromName(typeName)) {
-                    if (type.IsSubclassOf(typeof(ScriptableObject)))
-                        return type;
-                }
+            
+            foreach (var type in TypeUtils.GetTypesFromName(selectedAsset.name)) {
+                if (type.IsSubclassOf(typeof(ScriptableObject)))
+                    return type;
             }
 
             return null;
-        }
-    
-        private static List<string> GetAllTypeNamesIn(TextAsset selectedAsset) {
-            string fileText = selectedAsset.text;
-
-            Regex namespaceRegex = new Regex(@"(?<!@)namespace\s+(?<name>\S*)\s*{");
-            Regex classNameRegex = new Regex(@"(?<!@)class\s+(?<name>\S*)\s*");
-
-            MatchCollection namespaceMatches = namespaceRegex.Matches(fileText);
-            List<string> namespaces = (from Match namespaceMatch in namespaceMatches select namespaceMatch.Groups["name"].Value).ToList();
-        
-            MatchCollection classNameMatches = classNameRegex.Matches(fileText);
-            List<string> classNames = (from Match classNameMatch in classNameMatches select classNameMatch.Groups["name"].Value).ToList();
-
-            if (namespaceMatches.Count == 0)
-                return classNames;
-        
-            List<string> output = new List<string>();
-            foreach (var className in classNames) {
-                foreach (var ns in namespaces) {
-                    output.Add($"{ns}.{className}");
-                }
-            }
-
-            return output;
         }
 
         private static void CreateAsset(string folderPath, Type assetType) {
